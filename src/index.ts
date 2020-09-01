5b860d4da9da5b024c4f5c43d7f125f7ac579bf4
Jordan Cervantes <jordan.cervantes@yahoo.com>
2020-09-01 17:31:08 -0400

    Configuring express. Setting up app on localhost 3000

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,21 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express'
 
 const main = async () => {
-
     const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
 
-    const post = orm.em.create(Post, {title: 'my first post'});
-    await orm.em.persistAndFlush(post);
-    console.log('---------------------sql 2--------------------');
-    await orm.em.nativeInsert(Post, {title: 'my first post2'})
+    const app = express();
+    app.get('/', (_, res) => {
+        res.send('hello')
+
+    })
+    app.listen(3000, () => {
+        console.log('server started on localhost:3000')
+    })
 }
 
-main()
+main();
 
